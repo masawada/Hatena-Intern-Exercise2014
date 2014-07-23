21cd9e7f02b9992ba93ed14b5a9a3164d81adcec
Masayoshi Wada <developer@andantesoftware.com>
2014-07-23 15:21:32 +0900

    課題 Perl-1 完了
    + とりあえずmain.plは期待通りに動いている
    + テスト通った

--- perl/lib/Log.pm
+++ perl/lib/Log.pm
@@ -1,6 +1,10 @@
 package Log;
 use strict;
 use warnings;
+use POSIX;
+
+# Timezone
+$ENV{'TZ'} = "JST";
 
 sub new {
     my ($class, %args) = @_;
@@ -8,18 +12,31 @@ sub new {
 }
 
 sub protocol {
+  my $self = shift;
+  my @req = split(/ /, $self->{req});
+  return $req[2];
 }
 
 sub method {
+  my $self = shift;
+  my @req = split(/ /, $self->{req});
+  return $req[0];
 }
 
 sub path {
+  my $self = shift;
+  my @req = split(/ /, $self->{req});
+  return $req[1];
 }
 
 sub uri {
+  my $self = shift;
+  return "http://" .  $self->{host} . $self->path
 }
 
 sub time {
+  my $self = shift;
+  return POSIX::strftime "%Y-%m-%dT%H:%M:%S", localtime($self->{epoch});
 }
 
 1;
