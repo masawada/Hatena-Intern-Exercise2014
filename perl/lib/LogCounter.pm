c12fb47a7e85df3e3d929d2e0e9b5ed31fc63d88
Masayoshi Wada <developer@andantesoftware.com>
2014-07-24 00:38:19 +0900

    課題 Perl-3 ひとまず完了
    + テスト通った

--- perl/lib/LogCounter.pm
+++ perl/lib/LogCounter.pm
@@ -3,14 +3,34 @@ use strict;
 use warnings;
 
 sub new {
-    my ($class, $logs) = @_;
-    return bless { logs => $logs }, $class;
+  my ($class, $logs) = @_;
+  return bless { logs => $logs }, $class;
 };
 
 sub group_by_user {
+  my $self = shift;
+  my $result = {};
+
+  for my $log (@{$self->{logs}}) {
+    my $user = ($log->{user})? $log->{user} : "guest";
+    if (!$result->{$user}) { $result->{$user} = []; }
+    push($result->{$user}, $log);
+  }
+
+  return $result;
 }
 
 sub count_error {
+  my $self = shift;
+  my $result = 0;
+
+  for my $log (@{$self->{logs}}) {
+    if ($log->{status} =~ /^5\d{2}$/) {
+      $result++;
+    }
+  }
+
+  return $result;
 }
 
 1;
