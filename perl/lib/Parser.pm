0f421b101c100969adc9081d412e74bf85a1d1f7
Masayoshi Wada <developer@andantesoftware.com>
2014-07-23 15:24:43 +0900

    とりあえず課題2を実装
    + main2.plは期待通りに動いている
    + テストが通らない
    ++ Logにto_hashメソッドがない？

--- perl/lib/Parser.pm
+++ perl/lib/Parser.pm
@@ -1,6 +1,7 @@
 package Parser;
 use strict;
 use warnings;
+use Log;
 
 sub new {
     my ($class, %args) = @_;
@@ -8,6 +9,25 @@ sub new {
 }
 
 sub parse {
-}
+  my $self = shift;
+  my $filename = $self->{filename};
+  my @result;
+
+  open my $fh, '<' , $filename or die $!;
+  my @lines = <$fh>;
+
+  for my $line (@lines) {
+    chomp($line);
+
+    push(@result, Log->new(
+      map{
+        my ($key, $val) = split(/:/, $_, 2);
+        ($val ne "-")? ($key => $val) : ();
+      } split /\t/, $line)
+    );
+  }
+
+  return \@result;
+} 
 
 1;
