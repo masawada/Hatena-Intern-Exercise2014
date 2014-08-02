a8b19b16ade11ef73829339461654e6c49a7fdb9
Masayoshi Wada <developer@andantesoftware.com>
2014-08-02 10:22:04 +0900

    課題 JS-3 実装した

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,17 @@
 // 課題 JS-3 の実装をここに記述してください。
+(function(){
+  var apply_button = document.querySelector('#submit-button');
+  var removeChildren = function (dst) {
+    while (dst.childNodes.length) {
+      dst.removeChild(dst.firstChild);
+    }
+  };
+
+  apply_button.addEventListener('click', function () {
+    var dst = document.querySelector('#table-container');
+    var logs = parseLTSVLog(document.querySelector('#log-input').value);
+
+    removeChildren(dst);
+    createLogTable(dst, logs);
+  });
+})();
