456d6565b671556f0f39a890d0bb62cac55f5655
Masayoshi Wada <developer@andantesoftware.com>
2014-08-06 23:42:52 +0900

    フィルタ機能が完成しました

--- js/js-4.js
+++ js/js-4.js
@@ -2,6 +2,30 @@
 
   'use strict';
 
+  // filters
+  var filters = {
+    'all': function() {
+      return function() {
+        return true;
+      };
+    },
+    'path': function(data) {
+      return function(record) {
+        return record.path.indexOf(data) !== -1;
+      };
+    },
+    'lte_reqtime': function(data) {
+      return function(record) {
+        return Number(data) <= record.reqtime_microsec;
+      };
+    },
+    'gte_reqtime': function(data) {
+      return function(record) {
+        return Number(data) >= record.reqtime_microsec;
+      };
+    },
+  };
+
   // main
   var app = new Vue({
     el: '#app',
@@ -11,14 +35,15 @@
       filterType: [
         {type: 'all', label: 'All'},
         {type: 'path', label: 'Path'},
-        {type: 'lt_microsec', label: '<= Req Time'},
-        {type: 'gt_microsec', label: '>= Req Time'}
+        {type: 'lte_reqtime', label: '<= Req Time'},
+        {type: 'gte_reqtime', label: '>= Req Time'}
       ],
       currentFilterType: {type: 'all', label: 'All'},
       tabs: [
         {id: '#input-tab', label: 'Input', isActive: true},
         {id: '#search-tab', label: 'Search', isActive: false}
-      ]
+      ],
+      filter_data: ''
     },
     ready: function () {
       // set default data
@@ -28,6 +53,11 @@
                 'path:/help	reqtime_microsec:234222\n'+
                 'path:/	reqtime_microsec:94843\n');
       this.reloadRecords();
+    },
+    filters: {
+      filterRecords: function (records) {
+        return records.filter(filters[this.currentFilterType.type](this.$get('filter_data')));
+      }
     },
     methods: {
       selectTab: function (target) {
@@ -47,4 +77,6 @@
       }
     }
   });
+
+  app.filters = filters;
 })();
